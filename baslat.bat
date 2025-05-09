65139c1c71ac3cec3be654c5ef6354213d812365
eymownfo <eymendiscorx@gmail.com>
2025-05-09 16:27:49 +0300

    gkmgk
    fkjtgjk

---
+++ baslat.bat
@@ -0,0 +1,7 @@
+@echo off
+title SesBotu
+color 9
+:run
+node .
+echo Bot Coktu Yeniden Baslatiliyor
+goto run
---
+++ kurulum.bat
@@ -0,0 +1,10 @@
+@echo off
+title SesBotu Kurulum
+color 9
+echo.
+echo config.json dosyasini doldurmayi unutmayin !
+echo doldurdu iseniz bir tuşa basarak devam edebilirsiniz.
+pause
+
+npm install
+
